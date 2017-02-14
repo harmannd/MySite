156d6775fa8daef9c1b8ae65eb08ee6fc5991115
harmannd <derekhar2468@gmail.com>
2017-02-14 16:04:23 -0600

    Intial website design with rolodex fully functional

---
+++ MySite/Controllers/ContactsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using MySite.Models;
+
+namespace MySite.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private MySiteDatabaseEntities db = new MySiteDatabaseEntities();
+
+        // GET: Contacts
+        public ActionResult Index() {
+            return View(db.Contacts.ToList());
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(Guid? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null) {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Address,City,State,Phone,Email,Notes,Zipcode")] Contact contact) {
+            if (ModelState.IsValid) {
+                contact.ContactID = Guid.NewGuid();
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(Guid? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null) {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ContactID,Name,Address,City,State,Phone,Email,Notes,Zipcode")] Contact contact) {
+            if (ModelState.IsValid) {
+
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(Guid? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null) {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id) {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MySite/Models/ContactMetadata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MySite.Models {
+    [MetadataType(typeof(Contact.Metadata))]
+    public partial class Contact {
+        sealed class Metadata {
+            [Key]
+            public System.Guid ContactID { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            [DisplayFormat(ConvertEmptyStringToNull = false)]
+            public string Address { get; set; }
+
+            [DisplayFormat(ConvertEmptyStringToNull = false)]
+            public string City { get; set; }
+
+            public int? State { get; set; }
+
+            [Required]
+            public string Phone { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+
+            [DisplayFormat(ConvertEmptyStringToNull = false)]
+            public string Notes { get; set; }
+
+            [DisplayFormat(ConvertEmptyStringToNull = false)]
+            [RegularExpression(@"^\d{5}(?:[-\s]\d{4})?$", ErrorMessage = "Please enter a valid 5 digit zip code.")]
+            public string Zipcode { get; set; }
+        }
+    }
+}
